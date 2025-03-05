020eecd2fb2896238e2087b130873f839aba0958
Sode, Adedamola (DLSLtd,RAL,LSCI) <adedamola.sode@diamond.ac.uk>
2025-03-05 17:01:14 +0000

    Changed to from datetime to time, and added changes

--- client_send_test.py
+++ client_send_test.py
@@ -1,6 +1,6 @@
 import asyncio
-import datetime
 import sys
+import time
 
 from pandablocks.asyncio import AsyncioClient
 from pandablocks.commands import Put
@@ -8,39 +8,42 @@ from pandablocks.commands import Put
 TIME_LIST = []
 
 
-def timer(func):
-    async def process(func, *args, **params):
-        return await func(*args, **params)
+# def timer(func):
+#     async def process(func, *args, **params):
+#         return await func(*args, **params)
 
-    async def helper(*args, **params):
-        print(f"{func.__name__}.time")
-        start = datetime.datetime.now()
-        print(start)
+#     async def helper(*args, **params):
+#         print(f"{func.__name__}.time")
+#         start = datetime.datetime.now()
+#         print(start)
 
-        result = await process(func, *args, **params)
+#         result = await process(func, *args, **params)
 
-        finish = datetime.datetime.now()
+#         finish = datetime.datetime.now()
 
-        TIME_LIST.append(finish - start)
+#         TIME_LIST.append(finish - start)
 
-        print(">>> Time elapsed - ", finish - start)
-        return result
+#         print(">>> Time elapsed - ", finish - start)
+#         return result
 
-    return helper
+#     return helper
 
 
-@timer
 async def send_pgen():
     # Create a client and connect the control and data ports
     async with AsyncioClient(sys.argv[1]) as client:
         # Put to 2 fields simultaneously
-        await asyncio.gather(
-            client.send(Put("BITS.A", 1)),
-        )
+        for x in range(0, 10):
+            start = time.time()
+            print(start)
+            await asyncio.gather(
+                client.send(Put("BITS.A", 1)),
+            )
+            finish = time.time()
+            TIME_LIST.append(finish - start)
 
 
 if __name__ == "__main__":
-    for x in range(0, 10):
-        asyncio.run(send_pgen())
+    asyncio.run(send_pgen())
 
     print(TIME_LIST)
